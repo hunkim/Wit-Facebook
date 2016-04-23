0e093212e9fe22eaa1b2d590d19371abfb1aed13
hunkim <hunkim@gmail.com>
2016-04-23 15:09:51 +0800

    Added all const_tests

--- __tests__/const_test.js
+++ __tests__/const_test.js
@@ -1,9 +1,37 @@
 jest.dontMock('../const.js');
 
-describe('Const setting test', function() {
-  it('WIT_TOKEN test'), function() {
-    process.env.WIT_TOKEN = 'wit_token';
-    var config = require('../const.js');
-    expect(config.WIT_TOKEN).toBe('wit_token');
-  };
+describe('Const setting test', () => {
+	/*
+	it('No WIT_TOKEN test',  () => {
+		require('../const.js');
+    	expect(config.WIT_TOKEN).toThrow(new Error('missing WIT_TOKEN'));
+  	});*/
+
+ 	it('WIT_TOKEN test', () => {
+    	process.env.WIT_TOKEN = 'wit_token';
+    	var config = require('../const.js');
+    	expect(config.WIT_TOKEN).toEqual('wit_token');
+  	});
+
+  	it('No FB_PAGE_TOKEN test', () => {
+    	var config = require('../const.js');
+    	expect(config.FB_PAGE_TOKEN).toEqual(null);
+  	});
+
+  	it('No FB_PAGE_TOKEN test', () => {
+    	process.env.FB_PAGE_TOKEN = 'fb_token';
+    	var config = require('../const.js');
+    	expect(config.FB_PAGE_TOKEN).toEqual('fb_token');
+  	});
+
+	it('No FB_VERIFY_TOKEN test', () => {
+    	var config = require('../const.js');
+    	expect(config.FB_VERIFY_TOKEN).toEqual('just_do_it');
+  	});
+
+  	it('No FB_VERIFY_TOKEN test', () => {
+    	process.env.FB_VERIFY_TOKEN = 'fb_1234';
+    	var config = require('../const.js');
+    	expect(config.FB_VERIFY_TOKEN).toEqual('fb_1234');
+  	});  	
 });
